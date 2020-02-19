a70530b7b36164df4dfd4b15d5ea8c0600eff8cb
sortweste <00123216@uca.edu.sv>
2020-02-19 12:02:00 -0600

    Working in Styles!

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.delete_all
+
+articles = [['React', 'La mejor tecnología para desarrollar en Front End!'],
+            ['Flutter','Debes aprender esta tecnología, no tienes opción!'],
+            ['React Native vs Flutter', '¿Quién ganará la batalla del desarrollo móvil híbrido?'],
+            ['Vue.js', 'Descubre por qué aprender esta tecnología'],
+            ['Código Abierto', 'El comienzo de Machine Learning'],
+            ['Prolog', 'El lenguaje por sobre todos los lenguajes'],
+            ['Racket', 'Aprende la Programación Funcional'],
+            ['ICPC 2020', '¿Vale la pena inscribirse?'],
+            ['Hooks', 'Una nueva forma de state']]
+
+
+articles.each do |title, description|
+  Article.create(title: title, description: description)
+end
